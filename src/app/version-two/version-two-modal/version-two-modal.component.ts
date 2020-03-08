87b5a50bccbb9c4ad08a7972d164258ad58e72e1
Hrvoje T <hrvooje@gmail.com>
2020-03-08 14:32:01 +0100

    Added to DetailsModal doubleDamageTo, doubleDamageFrom, halfDamageTo, halfDamageFrom

--- src/app/version-two/version-two-modal/version-two-modal.component.ts
+++ src/app/version-two/version-two-modal/version-two-modal.component.ts
@@ -39,17 +39,46 @@ export class VersionTwoModalComponent implements OnInit {
       this.type.forEach(element => {
         this.pokemonRestService.getData(element.type.url).subscribe((response: any) => {
         console.log("response getData");
-        
+
           //Double Damage To
           const ddt = response.damage_relations.double_damage_to;
           console.log("ddt-->", ddt)
           ddt.forEach(element => {
             console.log("element u ddt-u-->", element);
             this.doubleDamageTo.push(element.name);
-            console.log("this.doubleDamageTo", this.doubleDamageTo);
+            console.log("1this.doubleDamageTo", this.doubleDamageTo);
           });
-          this.doubleDamageTo = this.doubleDamageTo.filter((el, i, a) => i === a.indexOf(el))
-          console.log("this.doubleDamageTo", this.doubleDamageTo);
+          // this.doubleDamageTo = this.doubleDamageTo.filter((el, i, a) => i === a.indexOf(el))
+          // console.log("2this.doubleDamageTo", this.doubleDamageTo);
+
+          //Double Damage From
+          const ddf = response.damage_relations.double_damage_from;
+          console.log("ddf-->", ddf)
+          ddf.forEach(element => {
+            console.log("element u ddf-u-->", element);
+            this.doubleDamageFrom.push(element.name);
+            console.log("1this.doubleDamageFrom", this.doubleDamageFrom);
+          });
+
+
+          //Half Damage To
+          const hdt = response.damage_relations.half_damage_to;
+          console.log("hdt-->", hdt)
+          hdt.forEach(element => {
+            console.log("element u hdt-u-->", element);
+            this.halfDamageTo.push(element.name);
+            console.log("1this.halfDamageTo", this.halfDamageTo);
+          });
+
+          //Half Damage From
+          const hdf = response.damage_relations.half_damage_from;
+          console.log("hdf-->", hdf)
+          hdf.forEach(element => {
+            console.log("element u hdf-u-->", element);
+            this.halfDamageFrom.push(element.name);
+            console.log("1this.halfDamageFrom", this.halfDamageFrom);
+          });
+
         });
       });
 
